f558e4103ba4ed6fdaef02c87396f38bf56f9a5a
Rurika Fuyuki <181914382+RURIKA0221@users.noreply.github.com>
2024-11-06 21:49:16 +0900

    11/6時点

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -5,7 +5,10 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.demo.data.entity.User;
@@ -42,7 +45,13 @@ public class UserController {
 
 	// マッピング設定
 	@PostMapping("/newuser")
-	public String registerUser(UserForm userForm) {
+	// 引数にBindingResultを追加
+	public String registerUser(@Validated UserForm userForm, BindingResult bindingResult) {
+		// バリデーションの結果、エラーがあるかどうかチェック
+		if (bindingResult.hasErrors()) {
+			// エラーがある場合はユーザー登録画面を返す
+			return "newuser";
+		}
 		User user = new User();
 		user.setName(userForm.getName());
 		user.setEmail(userForm.getEmail());
@@ -52,4 +61,14 @@ public class UserController {
 		// ユーザ一覧画面へリダイレクト
 		return "redirect:/users";
 	}
+	
+	// deleteUserメソッドを追加
+		// リクエストマッピング設定を追加
+		@PostMapping("/users/delete/{id}")
+		// 処理の中でidを使えるように、引数にidを追加
+		public String deleteUser(@PathVariable Long id) {
+			// 指定したIDのユーザーを削除
+			userRepository.deleteById(id);
+			return "redirect:/users";
+		}
 }
--- src/main/java/com/example/demo/form/UserForm.java
+++ src/main/java/com/example/demo/form/UserForm.java
@@ -1,11 +1,18 @@
 package com.example.demo.form;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Size;
+
 import lombok.Data;
 
 @Data
 public class UserForm {
+	// nameへのバリデーション設定を追加
+		@Size(min = 1, max = 200)
+		private String name;
 
-    private String name;
-
-    private String email;
+		// emailへのバリデーション設定を追加
+		@Size(min = 1, max = 200)
+		@Email
+		private String email;
 }
